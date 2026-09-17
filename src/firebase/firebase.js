5964f21e305e40411972e31ef7dec1ce1404e0e8
Brian Cheptiony <cheptiony6@gmail.com>
2026-09-17 08:09:07 +0300

    updated .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0pBlw1ordJ0mzveQBihhknD9d1Jaxys0",
-  authDomain: "thriftyard-1.firebaseapp.com",
-  projectId: "thriftyard-1",
-  storageBucket: "thriftyard-1.firebasestorage.app",
-  messagingSenderId: "563611869667",
-  appId: "1:563611869667:web:2bca49ec594cdd753980d6",
-  measurementId: "G-RC2TGCRHVE",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
